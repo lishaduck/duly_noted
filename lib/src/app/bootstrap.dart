ff1f6238929b1b59038fbc271bbe9d63120025d7
Eli <88557639+lishaduck@users.noreply.github.com>
2024-04-26 15:24:53 -0500

    feat: more bootstrapping

--- lib/src/app/bootstrap.dart
+++ lib/src/app/bootstrap.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart' as flutter show runApp;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,13 +16,29 @@ mixin Bootstrap on Widget {
   /// Bootstrap the app.
   ///
   /// This involves
+  /// - setting [FlutterError.onError] to log errors to the console,
+  /// - calling [usePathUrlStrategy] to use path-style URLs,
   /// - setting up the [SettingsController] and loading the user's preferences,
-  /// - initializing riverpod's [ProviderScope],
+  /// - initializing riverpod's [ProviderScope], and
   /// - running the app with [RunApp].
   Future<void> bootstrap(
     RunApp runApp,
     Future<SharedPreferences> Function() getSharedPreferences,
   ) async {
+    FlutterError.onError = (details) =>
+        log(details.exceptionAsString(), stackTrace: details.stack);
+
+    // Don't use hash style routes.
+    usePathUrlStrategy();
+
+    // Reset notification bar on Android.
+    WidgetsFlutterBinding.ensureInitialized();
+    await SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top],
+    );
+
+    // Load the user's preferences.
     final prefs = await getSharedPreferences();
     final initialSettings = await loadSettings(prefs);
 
