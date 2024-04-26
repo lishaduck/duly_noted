c5e938809a48eba54791c65f5d5807418467a2d5
Eli <88557639+lishaduck@users.noreply.github.com>
2024-04-26 08:36:42 -0500

    chore: bring over PSDTools/app
    I had a lot of custom files, this merges them in.
    I also made a logo, and set up a splash screen.

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -7,7 +7,7 @@ import 'settings_controller.dart';
 /// When a user changes a setting, the SettingsController is updated and
 /// Widgets that listen to the SettingsController are rebuilt.
 class SettingsView extends StatelessWidget {
-  const SettingsView({super.key, required this.controller});
+  const SettingsView({required this.controller, super.key});
 
   static const routeName = '/settings';
 
@@ -42,7 +42,7 @@ class SettingsView extends StatelessWidget {
             DropdownMenuItem(
               value: ThemeMode.dark,
               child: Text('Dark Theme'),
-            )
+            ),
           ],
         ),
       ),
